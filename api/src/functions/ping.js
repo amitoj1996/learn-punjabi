8e1558b45c055b1d2fe7a1d22d59855ab8eb9144
Amitoj Singh Lotey <amitoj.9896@gmail.com>
2025-12-14 21:07:06 -0500

    chore(debug): add ping endpoint to test function deployment

---
+++ api/src/functions/ping.js
@@ -0,0 +1,10 @@
+const { app } = require('@azure/functions');
+
+app.http('ping', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    route: 'ping',
+    handler: async (request, context) => {
+        return { body: "pong" };
+    }
+});
