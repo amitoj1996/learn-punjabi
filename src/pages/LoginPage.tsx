091ebc0e314da352d5a0a61eecc109bfa24198f3
Amitoj Singh Lotey <amitoj.9896@gmail.com>
2025-12-24 01:05:30 -0800

    chore: Revert to fd81f23 and remove Microsoft login option - Google only

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -20,18 +20,6 @@ const providers: AuthProvider[] = [
             </svg>
         ),
         url: '/.auth/login/google',
-    },
-    {
-        name: 'Microsoft',
-        icon: (
-            <svg className="w-5 h-5" viewBox="0 0 24 24">
-                <path fill="#F25022" d="M1 1h10v10H1z" />
-                <path fill="#00A4EF" d="M1 13h10v10H1z" />
-                <path fill="#7FBA00" d="M13 1h10v10H13z" />
-                <path fill="#FFB900" d="M13 13h10v10H13z" />
-            </svg>
-        ),
-        url: '/login/microsoft',
     }
 ];
 
